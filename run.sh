86d0b2d2fd41a6fad104d75832eed213e6de546c
xeyossr <113219171+xeyossr@users.noreply.github.com>
2024-10-22 01:10:41 +0400

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,10 @@
 source /etc/os-release
 
 if [[ "$ID" == "arch" || "$ID_LIKE" == "arch" ]]; then
-    for module in "${modules[@]}"; do
-        yay -S --noconfirm python-customtkinter python-pystray python-psutil python-pillow tk 
-    done
+    yay -S --noconfirm python-customtkinter python-pystray python-psutil python-pillow tk 
+
 else
-    for module in "${modules[@]}"; do
-        pip install customtkinter pystray psutil pillow tk --break-system-packages
-    done
+    pip install customtkinter pystray psutil pillow tk --break-system-packages
 fi
+
+python easylinuxvpn.py
